ae3814bf7c3b695d9b5aead77f5edfe349212c50
Prashanta chowdhury <126135485+Prashanta1999@users.noreply.github.com>
2023-10-09 19:42:50 +0530

    Update Axis.py
    I added title

--- Visualization Codes/Axis.py
+++ Visualization Codes/Axis.py
@@ -1,8 +1,10 @@
 #Code to plot axis of a graph 
-
+import numpy as np
 from matplotlib import pyplot as plt
 
-
-plt.plot([1,2,3,4],[1,4,9,16],'ro')
+plt.plot(np.array([1,2,3,4]),'ro')
+# plt.plot(np.array([[1,2,3,4],[1,4,9,16]]),'ro')
 plt.axis([0,6,0,20])
+plt.title("Axis")
+plt.plot(np.array([[1,2,3,4],[1,4,9,16]]),'ro')
 plt.show()
